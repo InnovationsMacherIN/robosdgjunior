f31948ad05d76991ba98a834b90245fca9408322
Anniina <anniina.rytkonen90@gmail.com>
2025-01-25 02:19:09 +0200

    Graphical changes v002

--- src/config/blockIconConfig.jsx
+++ src/config/blockIconConfig.jsx
@@ -7,17 +7,22 @@ import icon_nuoli_taakse from "../assets/icons/robo-arrow-backwards.svg";
 import icon_puoli from "../assets/icons/puoli-pallo.svg";
 import icon_rengas_vasen from "../assets/icons/robo-tyre-left.svg";
 import icon_rengas_oikea from "../assets/icons/robo-tyre-right.svg";
+import icon_start from "../assets/icons/robo-play-icon.svg";
+import icon_displayoff from "../assets/icons/robo-display-turn-off.svg";
+import icon_emoji_love from "../assets/icons/robo-emoji-love.svg";
 
 const BlockIconConfig = ({ blockId }) => {
 
   const getIconSrc = (blockId) => {
     switch (blockId) {
       case 'start':
-        return '';
+        return 'icon_start';
       case 'end':
         return '';
       case 'melody':
+        return icon_emoji_love;
       case 'sound':
+        return icon_emoji_love;
         return icon_puoli;
       case 'forward':
         return icon_nuoli;
@@ -27,6 +32,8 @@ const BlockIconConfig = ({ blockId }) => {
         return icon_rengas_vasen;
       case 'turn-right':
         return icon_rengas_oikea;
+      case 'leds-off':
+        return icon_displayoff;
       // Add more cases for other block IDs
       default:
         return ''; // Default icon or empty string if no icon

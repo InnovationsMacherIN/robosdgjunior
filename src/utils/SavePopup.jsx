f553833d1447b6588c22ec3c0ec691c5870dc782
Atte Haataja <atte@kaswe.fi>
2025-03-11 15:52:03 +0900

    Save pop up updated

--- src/utils/SavePopup.jsx
+++ src/utils/SavePopup.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createPortal } from 'react-dom';
 import '../styles/SavePopup.css';
+import RoboClose from '../assets/icons/robo-close.jsx';
 
 const SavePopup = ({ onClose, onDownload, onUpload }) => {
   const handleFileChange = (event) => {
@@ -19,9 +20,14 @@ const SavePopup = ({ onClose, onDownload, onUpload }) => {
     <div className="save-popup-container">
       <div className="popup-overlay" onClick={onClose} />
       <div className="save-popup">
-        <button classname="save-popup-download-button" onClick={onDownload}>Download</button>
-        <input type="file" accept=".json" onChange={handleFileChange} />
-        <button onClick={onClose}>Close</button>
+        <button className="close-button" onClick={onClose}>
+          <RoboClose style={{ width: '26px', height: '26px', color: 'white' }} />
+        </button>
+        <div className="save-popup-content">
+          <button className="save-popup-button" onClick={onDownload}>Download</button>
+          <input type="file" accept=".json" onChange={handleFileChange} />
+          <button className="save-popup-button" onClick={onClose}>Close</button>
+        </div>
       </div>
     </div>,
     document.body
